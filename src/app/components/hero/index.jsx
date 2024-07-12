af35355ed0d9910c4188ed31bce7de072c9d762d
harzyz <abdulazeezahmed85@gmail.com>
2024-07-12 12:41:04 -0700

    latest

--- src/app/components/hero/index.jsx
+++ src/app/components/hero/index.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 import styles from "../hero/hero.module.scss";
-import NavBar from '../navbar';
+import NavBar from "../navbar";
 
 export default function Hero() {
   return (
     <div className={styles.hero_container}>
-        <NavBar />
-        <div className={styles.rogue_image}>
+      <NavBar />
+      <div className={styles.rogue_image}>
         <section className={styles.Unveil_wrapper}>
-            <h5>Let’s help you</h5>
-            <h2>Unveil <span>efficiency,</span> elevate <span>productivity.</span> Experience Rogue’s design today</h2>
-            <p>Leading digital agency with solid design and development expertise</p>
-            <button>Let&apos;s talk</button>
+          <h5>Let&apos;s help you</h5>
+          <h2>
+            Unveil <span>efficiency,</span> elevate <span>productivity.</span>{" "}
+            Experience Rogue&apos;s design today
+          </h2>
+          <p>
+            Leading digital agency with solid design and development expertise
+          </p>
+          <button>Let&apos;s talk</button>
         </section>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
